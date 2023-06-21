729ad877e01e7954b5fee07e96b9d33341c27a69
Lenon <lenoninformatica@gmail.com>
2023-06-21 18:06:23 -0300

    Feat: Rota de partidas refatorada, nao estava passando nos testes

--- app/backend/src/controllers/MatchsController.ts
+++ app/backend/src/controllers/MatchsController.ts
@@ -1,12 +1,12 @@
 import { Request, Response } from 'express';
-import MatchsSerice from '../services/MatchsSerice';
+import MatchsSerice from '../services/MatchsService';
 
 class ControllerMatchs {
   private service = new MatchsSerice();
 
   async allMatcherController(req: Request, res: Response) {
     const { inProgress } = req.query;
-    if (inProgress) {
+    if (inProgress) { // se a chave inProgress existir
       return this.findQueryMatcherController(req, res);
     }
     const partidas = await this.service.allMatcherService();
@@ -15,7 +15,7 @@ class ControllerMatchs {
 
   async findQueryMatcherController(req: Request, res: Response) {
     const { inProgress } = req.query;
-    // refatorado na mentoria da manha é passando o inprogres no paramentro simula o IF
+    // refatorado na mentoria da manha, é passando o inprogres no paramentro simula o IF
     const partidasEmProgresso = await this.service.findQueryMatcherModel(inProgress === 'true');
     return res.status(200).json(partidasEmProgresso.data);
   }
--- app/backend/src/models/MatchesModel.ts
+++ app/backend/src/models/MatchesModel.ts
@@ -1,22 +1,30 @@
 import ModelSequelize from '../database/models/MatchsModel'; // Model de Partidas
+import ModelTimes from '../database/models/TeamsModel'; // Model de Times
 import { FuncoesMatches } from '../Interfaces/interfacesServices';
 
 class ModelMatchs implements FuncoesMatches {
-  private model = ModelSequelize;
+  private modelM = ModelSequelize;
+  private modeT = ModelTimes;
+
   /* private encrypter: Encrypter,
     private tokenGenerator: TokenGenerator, */
 
   async allMatcherModel(): Promise<ModelSequelize[]> {
-    const todasPartidas = await this.model.findAll();
+    const todasPartidas = await this.modelM.findAll({
+      include: [
+        { model: ModelTimes, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: ModelTimes, as: 'awayTeam', attributes: { exclude: ['id'] } },
+      ] });
     return todasPartidas;
   }
 
   async findQueryMatcherModel(query: boolean): Promise<ModelSequelize[]> {
-    const partidaQuery = await this.model.findAll({
-      where: {
-        inProgress: query,
-      },
-    });
+    const partidaQuery = await this.modelM.findAll({
+      include: [
+        { model: ModelTimes, as: 'homeTeam', attributes: { exclude: ['id'] } },
+        { model: ModelTimes, as: 'awayTeam', attributes: { exclude: ['id'] } },
+      ],
+      where: { inProgress: query } });
     return partidaQuery;
   }
 }
